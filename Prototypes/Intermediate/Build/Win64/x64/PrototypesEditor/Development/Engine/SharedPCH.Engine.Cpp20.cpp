077c063f0cc610865e2cddcdcd8a9fc257e52005
Karolina Motužytė <224672@buas.nl>
2024-09-10 11:20:59 +0200

    Glitch prototype completed

---
+++ Prototypes/Intermediate/Build/Win64/x64/PrototypesEditor/Development/Engine/SharedPCH.Engine.Cpp20.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.36.32545
+#include "D:/GITHUB/Univertsity/Y3A-Prototypes/Prototypes/Intermediate/Build/Win64/x64/PrototypesEditor/Development/Engine/SharedPCH.Engine.Cpp20.h"
---
+++ Prototypes/Intermediate/Build/Win64/x64/PrototypesEditor/Development/Engine/SharedPCH.Engine.RTTI.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Engine/Public/EngineSharedPCH.h
+#include "D:/GITHUB/Univertsity/Y3A-Prototypes/Prototypes/Intermediate/Build/Win64/x64/PrototypesEditor/Development/Engine/SharedDefinitions.Engine.RTTI.Cpp20.h"
+#include "Runtime/Engine/Public/EngineSharedPCH.h"
